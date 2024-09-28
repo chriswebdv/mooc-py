765e517de5d91fea03981a63fc2eee5048cb26f7
chriswebdv <chriswebdv@gmail.com>
2024-09-28 12:02:05 -0600

    Concatenate variables and string

--- part-1.py
+++ part-1.py
@@ -5,3 +5,9 @@ name = input("What is your name? ")
 
 print(name)
 print(name)
+
+# Write your solution here
+#Concatenate variables and string
+name = input("What is your name? ")
+
+print("!" + name + "!" + name + "!")
