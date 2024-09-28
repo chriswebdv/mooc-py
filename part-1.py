ead1db138750eb3d903247868b56f9b4a71ee671
chriswebdv <chriswebdv@gmail.com>
2024-09-28 12:12:05 -0600

    ask for input from user and print info to screen

--- part-1.py
+++ part-1.py
@@ -13,3 +13,13 @@ name = input("What is your name? ")
 print("!" + name + "!" + name + "!")
 
 #name and address
+# Write your solution here
+
+first_name = input("Given name: ")
+last_name = input("Family name: ")
+address = input("Street address: ")
+city_and_code = input("City and postal code: ")
+
+print(first_name + " " + last_name)
+print(address)
+print(city_and_code)
