be41e55f95254138c547bceeda37e4ac46232a70
chriswebdv <chriswebdv@gmail.com>
2024-10-01 14:46:26 -0600

    Update part-1.py

--- part-1.py
+++ part-1.py
@@ -69,3 +69,13 @@ else:
     print(f"The absolute value of this number is {num}")
 
 # Order or magnitude by using conditional statements, based on user input
+# Write your solution here
+num = int(input("Please type in a number: "))
+
+if num < 1000:
+    print("This number is smaller than 1000")
+    if num < 100:
+        print("This number is smaller than 100")
+        if num < 10:
+            print("This number is smaller than 10")
+print("Thank you!")
