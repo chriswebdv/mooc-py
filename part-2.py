071d77603b72a4afdf92a0de528c7b32661ac40c
chriswebdv <chriswebdv@gmail.com>
2024-10-08 17:55:22 -0600

    compare 2 ints

--- part-2.py
+++ part-2.py
@@ -10,3 +10,13 @@ else:
     print("You are not of age!")
 
 # Compare 2 ints using conditionals
+num1 = int(input("Please type in the first number: "))
+num2 = int(input("Please type in another number: "))
+
+if num1 > num2:
+    print(f"The greater number was: {num1}")
+elif num2 > num1:
+    print(f"The greater number was: {num2}")
+else:
+    print("The numbers are equal!")
+
